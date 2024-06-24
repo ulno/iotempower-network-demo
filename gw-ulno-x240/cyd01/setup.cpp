179f0fdf63f3099c522ca896f96d1787902b011c
Ulrich Norbisrath <devel@mail.ulno.net>
2024-06-24 03:43:02 +0300

    added cyd

--- gw-ulno-x240/cyd01/setup.cpp
+++ gw-ulno-x240/cyd01/setup.cpp
@@ -1,4 +1,4 @@
-const char* id="01";
+const char* id="x240-01";
 
 rgb_single(led, ONBOARDLED_RED, ONBOARDLED_GREEN, ONBOARDLED_BLUE, true).off();
 button(b0, FLASHBUTTON, "pressed", "released").inverted().debounce(10);
@@ -9,7 +9,7 @@ analog(cover, LDR).filter_binarize(10,"covered","open");
 void start() {
     analogSetAttenuation(ADC_0db); // pretty esp32 specific, TODO: make available directly in IoTempower?
 	do_later(100, [] () {
-		IN(console).print("This is the cyd: x240-01")
+		IN(console).print("This is the cyd: ")
 			.println(id);
 	});
 }
