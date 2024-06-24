b3e1c54599ac19d56d50e300f897119f70b3a362
Ulrich Norbisrath <devel@mail.ulno.net>
2024-06-24 21:37:46 +0300

    add cyds and set right font on m5stick2

--- teacher/stick01/setup.cpp
+++ teacher/stick01/setup.cpp
@@ -3,7 +3,7 @@ const char* id="teacher-01";
 out(led, ONBOARDLED).inverted().off();
 button(home, BUTTON_HOME, "pressed", "released").inverted().debounce(10);
 button(buttom, BUTTON_RIGHT, "pressed", "released").inverted().debounce(10);
-m5stickc_display(console, 2, 270); // why does lcd as name turn into Lcd?
+m5stickc_display(console, 1, 270); // why does lcd as name turn into Lcd?
 m5stickc_imu(imu, true, true, false, false);
 
 void start() {
